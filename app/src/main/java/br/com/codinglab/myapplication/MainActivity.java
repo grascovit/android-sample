9eb195532326490d581af6640d94427888e42377
Gabriel Rascovit <rascovit@gmail.com>
2018-01-27 09:41:29 -0200

    extract retrofit to a separate client class

--- app/src/main/java/br/com/codinglab/myapplication/MainActivity.java
+++ app/src/main/java/br/com/codinglab/myapplication/MainActivity.java
@@ -1,19 +1,18 @@
 package br.com.codinglab.myapplication;
 
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import br.com.codinglab.myapplication.interfaces.LoginInterface;
+import br.com.codinglab.myapplication.interfaces.Login;
 import br.com.codinglab.myapplication.models.Auth;
 import br.com.codinglab.myapplication.models.LoginBody;
 import br.com.codinglab.myapplication.models.UserToken;
+import br.com.codinglab.myapplication.requests.ApiClient;
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,23 +20,8 @@ import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String API_URL = "https://clim-api-staging.herokuapp.com/api/v1/";
-    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
-    private static final Gson GSON = new GsonBuilder()
-            .setDateFormat(DATE_FORMAT)
-            .create();
-    private static final Retrofit API_CLIENT = new Retrofit.Builder()
-            .baseUrl(API_URL)
-            .addConverterFactory(GsonConverterFactory.create(GSON))
-            .build();
-
-    @BindView(R.id.mainActivity)
-    ConstraintLayout mainLayout;
-
     @BindView(R.id.editTextEmail)
     EditText email;
 
@@ -50,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
     @BindString(R.string.request_error)
     String requestError;
 
+    @BindString(R.string.login_dialog_title)
+    String loginDialogTitle;
+
+    @BindString(R.string.loading)
+    String loading;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,14 +48,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.buttonLogin)
-    void loginUser() {
+    public void loginUser() {
+        final ProgressBar progressBar = findViewById(R.id.progressBarLogin);
+        progressBar.setVisibility(View.VISIBLE);
+
         Auth authBody = new Auth(email.getText().toString(), password.getText().toString());
         LoginBody loginBody = new LoginBody(authBody);
-        LoginInterface loginService = API_CLIENT.create(LoginInterface.class);
-        Call<UserToken> call = loginService.loginUser(loginBody);
-        call.enqueue(new Callback<UserToken>() {
+        Login loginService = ApiClient.getLoginRequest();
+        loginService.loginUser(loginBody).enqueue(new Callback<UserToken>() {
             @Override
             public void onResponse(@NonNull Call<UserToken> call, @NonNull Response<UserToken> response) {
+                progressBar.setVisibility(View.GONE);
                 int statusCode = response.code();
 
                 if (statusCode == 201) {
@@ -77,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(@NonNull Call<UserToken> call, @NonNull Throwable t) {
+                progressBar.setVisibility(View.GONE);
                 Snackbar.make(findViewById(R.id.mainActivity), requestError, Snackbar.LENGTH_LONG).show();
             }
         });
---
+++ app/src/main/java/br/com/codinglab/myapplication/requests/ApiClient.java
@@ -0,0 +1,24 @@
+package br.com.codinglab.myapplication.requests;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import br.com.codinglab.myapplication.interfaces.Login;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static final String API_URL = "https://private-76ac7f-clim.apiary-mock.com/api/v1/";
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
+    private static final Gson GSON = new GsonBuilder()
+            .setDateFormat(DATE_FORMAT)
+            .create();
+    private static final Retrofit API_CLIENT = new Retrofit.Builder()
+            .baseUrl(API_URL)
+            .addConverterFactory(GsonConverterFactory.create(GSON))
+            .build();
+
+    public static Login getLoginRequest() {
+        return API_CLIENT.create(Login.class);
+    }
+}
